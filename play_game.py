9b763f025ec3dc12a11b70e6d62381a286ebde6a
allForOne-alt <weeesdom1@gmail.com>
2026-01-29 11:07:40 +0545

    NOW displays computer if computer wins!!!!!!!!

--- play_game.py
+++ play_game.py
@@ -1,31 +1,30 @@
 from noughtsandcrosses import *
 
-    
+
 def main():
-    board = [ ['1','2','3'],\
-              ['4','5','6'],\
-              ['7','8','9']]
+    board = [["1", "2", "3"], ["4", "5", "6"], ["7", "8", "9"]]
 
     welcome(board)
     total_score = 0
     while True:
         choice = menu()
-        if choice == '1':
+        if choice == "1":
             score = play_game(board)
-            total_score += score
-            print('Your current score is:',total_score)
-        if choice == '2':
+            if score == 1:
+                save_score(1)
+            elif score == -1:
+                save_score(1, "Computer")
+        if choice == "2":
             save_score(total_score)
-        if choice == '3':
+        if choice == "3":
             leader_board = load_scores()
             display_leaderboard(leader_board)
-        if choice == 'q':
+        if choice == "q":
             print('Thank you for playing the "Unbeatable Noughts and Crosses" game.')
-            print('Good bye')
+            print("Good bye")
             return
 
 
-    
 # Program execution begins here
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
